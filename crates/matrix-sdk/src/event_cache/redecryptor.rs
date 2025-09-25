f6770a3d4c78a24e78e0a2c89f800f9bb8f30aba
Damir Jelić <poljar@termina.org.uk>
2025-09-25 15:50:17 +0200

    feat(event_cache): Add the RedecryptorCtx concept

--- crates/matrix-sdk/src/event_cache/redecryptor.rs
+++ crates/matrix-sdk/src/event_cache/redecryptor.rs
@@ -25,15 +25,86 @@ use matrix_sdk_base::{
     deserialized_responses::{DecryptedRoomEvent, TimelineEvent, TimelineEventKind},
 };
 use matrix_sdk_common::executor::spawn;
-use ruma::{RoomId, serde::Raw};
+use ruma::{OwnedEventId, RoomId, events::AnySyncTimelineEvent, serde::Raw};
 use tokio::task::JoinHandle;
 use tokio_stream::wrappers::errors::BroadcastStreamRecvError;
-use tracing::{instrument, trace, warn};
+use tracing::{info, instrument, trace, warn};
 
 use crate::{
     Client,
     event_cache::{EventCache, EventCacheError, EventCacheInner},
 };
+
+pub(crate) trait RedecryptorCtx {
+    type Error;
+
+    async fn get_utds(
+        &self,
+        room_key_info: &RoomKeyInfo,
+    ) -> Result<Vec<(OwnedEventId, Raw<AnySyncTimelineEvent>)>, Self::Error>;
+
+    async fn on_resolved_utds(
+        &self,
+        room_id: &RoomId,
+        events: Vec<(OwnedEventId, DecryptedRoomEvent)>,
+    ) -> Result<(), Self::Error>;
+}
+
+impl RedecryptorCtx for EventCache {
+    type Error = EventCacheError;
+
+    async fn get_utds(
+        &self,
+        room_key_info: &RoomKeyInfo,
+    ) -> Result<Vec<(OwnedEventId, Raw<AnySyncTimelineEvent>)>, Self::Error> {
+        let only_utd_events = |event: TimelineEvent| {
+            // We need the event ID to be able to replace the event.
+            let event_id = event.event_id();
+            // We only care about events fort his particular room key, identified by the
+            // session ID.
+            let session_id = event.kind.session_id();
+
+            if session_id == Some(&room_key_info.session_id) {
+                // Only pick out events that are UTDs.
+                let event = as_variant!(event.kind, TimelineEventKind::UnableToDecrypt { event, .. } => event);
+                event_id.zip(event)
+            } else {
+                None
+            }
+        };
+
+        // Load the relevant events from the event cache store and attempt to redecrypt
+        // things.
+        //
+        // TODO: We can't load **all** events all the time.
+        let store = self.inner.store.lock().await?;
+        let events = store.get_room_events(&room_key_info.room_id).await?;
+
+        Ok(events.into_iter().filter_map(only_utd_events).collect())
+    }
+
+    async fn on_resolved_utds(
+        &self,
+        room_id: &RoomId,
+        events: Vec<(OwnedEventId, DecryptedRoomEvent)>,
+    ) -> Result<(), Self::Error> {
+        // Get the cache for this particular room and lock the state for the duration of
+        // the decryption.
+        let (room_cache, _) = self.for_room(room_id).await?;
+        let mut state = room_cache.inner.state.write().await;
+
+        for (event_id, decrypted) in events {
+            // The event isn't in the cache, nothing to replace. Realistically this can't
+            // happen since we retrieved the list of events from the cache itself.
+            if let Some((location, mut target_event)) = state.find_event(&event_id).await? {
+                target_event.kind = TimelineEventKind::Decrypted(decrypted);
+                state.replace_event_at(location, target_event).await?
+            }
+        }
+
+        Ok(())
+    }
+}
 
 pub(crate) struct Redecryptor {
     cache: Weak<EventCacheInner>,
@@ -66,57 +137,20 @@ impl Redecryptor {
     ) -> Result<(), EventCacheError> {
         trace!("Retrying to decrypt");
 
-        // Load the relevant events from the event cache store and attempt to redecrypt
-        // things.
-        let events = {
-            // TODO: We can't load **all** events all the time.
-            let store = cache.inner.store.lock().await?;
-            let events = store.get_room_events(&room_key_info.room_id).await?;
+        let events = cache.get_utds(&room_key_info).await?;
+        let mut decrypted_events = Vec::with_capacity(events.len());
 
-            events
-        };
-
-        let only_utd_events = |event: TimelineEvent| {
-            // We need the event ID to be able to replace the event.
-            let event_id = event.event_id();
-            // We only care about events fort his particular room key, identified by the
-            // session ID.
-            let session_id = event.kind.session_id();
-
-            if session_id == Some(&room_key_info.session_id) {
-                // Only pick out events that are UTDs.
-                let event = as_variant!(event.kind, TimelineEventKind::UnableToDecrypt { event, .. } => event);
-                event.zip(event_id)
-            } else {
-                None
-            }
-        };
-
-        // Get the cache for this particular room and lock the state for the duration of
-        // the decryption.
-        // TODO: Do we want to first decrypt and then do the locking and replacement,
-        // would be an additional loop but the lock would be free for the
-        // duration of the "slow" decryption attempts.
-        let (room_cache, _) = cache.for_room(&room_key_info.room_id).await?;
-        let mut state = room_cache.inner.state.write().await;
-
-        for (event, event_id) in events.into_iter().filter_map(only_utd_events) {
-            // The event isn't in the cache, nothing to replace. Realistically this can't
-            // happen since we retrieved the list of events from the cache itself.
-            let Some((location, mut target_event)) = state.find_event(&event_id).await? else {
-                continue;
-            };
-
+        for (event_id, event) in events {
             // If we managed to decrypt the event, and we should have to since we received
             // the room key for this specific event, then replace the event.
             if let Some(decrypted) =
                 self.decrypt_event(&cache, &room_key_info.room_id, event.cast_ref_unchecked()).await
             {
-                target_event.kind = TimelineEventKind::Decrypted(decrypted);
-
-                state.replace_event_at(location, target_event).await?
+                decrypted_events.push((event_id, decrypted));
             }
         }
+
+        cache.on_resolved_utds(&room_key_info.room_id, decrypted_events).await?;
 
         Ok(())
     }
@@ -166,5 +200,7 @@ impl Redecryptor {
                 todo!("Redecrypt all visible events?")
             }
         }
+
+        info!("Shutting down the event cache redecryptor");
     }
 }
